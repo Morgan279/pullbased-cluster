85230cc31a8cd1921306c100be0347e3f9ddd63d
ALiang <coder_wclong@163.com>
2021-09-18 00:15:06 +0800

    parameter

--- workspace-gateway/src/main/java/com/aliware/tianchi/constant/Config.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/constant/Config.java
@@ -4,6 +4,6 @@ public abstract class Config {
 
     public static final int SAMPLING_COUNT = 5000;
 
-    public static final int INITIAL_AVERAGE_RTT = 3;
+    public static final int INITIAL_AVERAGE_RTT = 1;
 
 }
