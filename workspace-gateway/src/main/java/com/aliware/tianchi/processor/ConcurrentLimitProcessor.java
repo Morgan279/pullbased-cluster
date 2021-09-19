fef6e97698f545f99d05dd574174e77e14aa1374
ALiang <coder_wclong@163.com>
2021-09-19 11:17:25 +0800

    parameter

--- workspace-gateway/src/main/java/com/aliware/tianchi/processor/ConcurrentLimitProcessor.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/processor/ConcurrentLimitProcessor.java
@@ -60,7 +60,7 @@ public class ConcurrentLimitProcessor {
         scheduledExecutorService.scheduleAtFixedRate(() -> RTPropEstimated = 44, WR, WR, TimeUnit.MILLISECONDS);
         scheduledExecutorService.scheduleAtFixedRate(() -> {
             if (congestion) {
-                this.gain = 0.01;
+                this.gain = 0.001;
                 this.status = ConcurrentLimitStatus.DRAIN;
                 scheduledExecutorService.schedule(() -> {
                     int round;
@@ -72,7 +72,7 @@ public class ConcurrentLimitProcessor {
                     this.status = ConcurrentLimitStatus.PROBE;
                 }, 10, TimeUnit.MILLISECONDS);
             }
-        }, 5000, 100, TimeUnit.MILLISECONDS);
+        }, 1000, 100, TimeUnit.MILLISECONDS);
     }
 
 
