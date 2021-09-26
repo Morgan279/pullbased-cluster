abb2f50a88b896d09b06b7f7144d607f53f0b3f7
ALiang <coder_wclong@163.com>
2021-09-26 13:53:31 +0800

    parameter

--- workspace-gateway/src/main/java/com/aliware/tianchi/processor/ConcurrentLimitProcessor.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/processor/ConcurrentLimitProcessor.java
@@ -61,7 +61,7 @@ public class ConcurrentLimitProcessor {
 
 
     public int getInflightBound() {
-        return (int) (gain * 0.1 * computingRateEstimate * RTPropEstimated * threads);
+        return (int) (gain * 0.5 * computingRateEstimate * RTPropEstimated * threads);
     }
 
 
