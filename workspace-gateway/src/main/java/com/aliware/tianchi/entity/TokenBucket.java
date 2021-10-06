d6fac670f5058d3a585d428e7e98e211ee771191
Morgan279 <appouksd.com@qq.com>
2021-10-07 00:56:27 +0800

    parameter

--- workspace-gateway/src/main/java/com/aliware/tianchi/entity/TokenBucket.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/entity/TokenBucket.java
@@ -43,7 +43,7 @@ public class TokenBucket {
         long now = (long) (elapsedNanos + (System.nanoTime() - lastAcquireNanoSec) / 1e3);
         synchronized (this) {
             if (ThreadLocalRandom.current().nextDouble() < 0.003 / RTPropEstimated) {
-                probe = 32;
+                probe = 48;
             }
             nextSendTime = (long) (now + waiting.get() / (pacingGain * probe * (computingRate / 1e3)));
         }
