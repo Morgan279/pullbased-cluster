eaf610c17fbea33210ce6b87bdf65a6b6deaa35b
Morgan279 <appouksd.com@qq.com>
2021-10-15 10:58:21 +0800

    update

--- workspace-gateway/src/main/java/com/aliware/tianchi/processor/ConcurrentLimitProcessor.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/processor/ConcurrentLimitProcessor.java
@@ -21,7 +21,7 @@ public class ConcurrentLimitProcessor {
 
     private static final int CW_FACTOR = 6;
 
-    private static final double[] GAIN_VALUES = {1.25, 0.9, 1, 1, 1, 1, 1, 1};
+    private static final double[] GAIN_VALUES = {1.25, 0.81, 1, 1, 1, 1, 1, 1};
 
     private final Object UPDATE_LOCK = new Object();
 
