79bcbd542b269be576343dfd54ef1e434cd41ac4
ALiang <coder_wclong@163.com>
2021-10-16 23:34:44 +0800

    update

--- workspace-gateway/src/main/java/com/aliware/tianchi/processor/ProbeProcessor.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/processor/ProbeProcessor.java
@@ -4,7 +4,7 @@ import com.aliware.tianchi.entity.ProbeRecord;
 
 public class ProbeProcessor {
 
-    private static final int UPPER_BOUND = 20;
+    private static final int UPPER_BOUND = 150;
 
     private static final int LOWER_BOUND = 1;
 
