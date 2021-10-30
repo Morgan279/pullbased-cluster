942ceecafa63ddbcd2c34bb78d825efcc754e4e8
ALiang <coder_wclong@163.com>
2021-10-30 23:46:56 +0800

    update

--- workspace-gateway/src/main/java/com/aliware/tianchi/entity/TimeWindow.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/entity/TimeWindow.java
@@ -2,40 +2,34 @@ package com.aliware.tianchi.entity;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TimeWindow {
 
-    private final int WINDOW_SIZE = 50;
+    private final int WINDOW_SIZE = 10;
 
     private final Deque<Double> deque = new ArrayDeque<>(WINDOW_SIZE);
 
     private AtomicInteger counter = new AtomicInteger(0);
 
-    private Map<Double, Integer> indexMap = new ConcurrentHashMap<>();
-
-    public TimeWindow() {
-        for (int i = 0; i < WINDOW_SIZE; ++i) {
-            deque.add(100D);
-        }
-    }
+    //private Map<Double, Integer> indexMap = new ConcurrentHashMap<>();
 
     public void addNewSample(double RTT) {
-        indexMap.put(RTT, counter.getAndIncrement());
+        counter.getAndIncrement();
+        //indexMap.put(RTT, counter.getAndIncrement());
         synchronized (deque) {
             while (!deque.isEmpty() && deque.peekLast() < RTT) {
                 deque.pollLast();
             }
-            deque.add(RTT);
-            if (counter.get() - indexMap.get(deque.peekFirst()) >= WINDOW_SIZE) {
+            if (counter.get() % WINDOW_SIZE == 0) {
                 deque.pollFirst();
             }
+            deque.addLast(RTT);
         }
     }
 
     public Double getMaxRTT() {
-        return deque.peekFirst();
+        return Optional.ofNullable(deque.peekFirst()).orElse(10D);
     }
 }
